81c8fa6d18b26c8671fb3ae599520b4478306e89
vishnup95 <vishnu.wish7@gmail.com>
2021-10-10 16:52:11 +0530

    updated the links

--- hidden-search/deploy.sh
+++ hidden-search/deploy.sh
@@ -4,15 +4,21 @@
 set -e
 
 # build
-npm run build
 
 # navigate into the build output directory
+mkdir dist
 cd dist
+git init
+git fetch git@personal:vishnup95/50-Days-50-Projects.git
+git pull git@personal:vishnup95/50-Days-50-Projects.git gh-pages --rebase
+rm -rf hidden-search
+npm run build
+
 
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
 
-git init
+
 git add -A
 git commit -m 'deploy'
 # git status
@@ -21,8 +27,7 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git fetch git@personal:vishnup95/50-Days-50-Projects.git
-git pull git@personal:vishnup95/50-Days-50-Projects.git gh-pages --rebase
+
 git push git@personal:vishnup95/50-Days-50-Projects.git master:gh-pages -f
 
 cd -
