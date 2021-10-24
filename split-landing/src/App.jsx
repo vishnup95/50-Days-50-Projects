5a39e6824c3a753f629a77f71b25d66af4b08dc6
vishnup95 <vishnu.wish7@gmail.com>
2021-10-24 16:20:00 +0530

    update

---
+++ split-landing/src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+	return (
+		<div className="app">
+			<header className="app-header">
+				<p>Welcome to our landing page</p>
+			</header>
+			<main>
+				<div className="grid-container">
+					<div className="wrapper left-wrapper"></div>
+					<div className="wrapper right-wrapper"></div>
+				</div>
+			</main>
+		</div>
+	);
+}
+
+export default App;
